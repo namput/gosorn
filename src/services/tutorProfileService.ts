ad84ff068af8131694f0e551da08f8f55cab112d
namput <47818793+namput@users.noreply.github.com>
2025-02-20 17:35:05 +0700

    updates

--- src/services/tutorProfileService.ts
+++ src/services/tutorProfileService.ts
@@ -2,7 +2,7 @@ const API_BASE_URL =
   import.meta.env.VITE_REACT_APP_API_URL || "http://localhost:5000"; // ✅ แก้พอร์ต Backend API
 export const submitTutorProfile = async (formData: FormData) => {
     try {
-      const response = await fetch(API_BASE_URL, {
+      const response = await fetch(`${API_BASE_URL}/tutor/create-profile`, {
         method: "POST",
         body: formData,
       });
